d0695d9477e89f55c9c0b27e9a27a25bb51fcf29
RomanIgambaev <roman.igambaev11@gmail.com>
2023-09-16 19:46:20 +0500

    filereader

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+        try {
+            fileReader = new FileReader("src/product list.csv");
+            bufferedReader = new BufferedReader(fileReader);
+            String line;
+            System.out.println("Список товарок");
+            bufferedReader.readLine();
+            while ((line = bufferedReader.readLine()) != null){
+                System.out.println(line);
+            }
+            bufferedReader.close();
+            fileReader.close();
+        }catch (IOException e){
+            try {
+                bufferedReader.close();
+                fileReader.close();
+            }catch (Exception ex){
+                System.out.println("Произошла ошибка");
+            }
+            throw new RuntimeException("Такой файл не найдет");
+        }
+
     }
 }
